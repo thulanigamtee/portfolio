4018e36965baf997ce780a90d8a844721f7ee78d
thulanigamtee <thulanigamtee@gmail.com>
2025-03-31 20:55:58 +0200

    add icons

--- src/app/icons.config.ts
+++ src/app/icons.config.ts
@@ -1,6 +1,24 @@
 import { provideIcons } from '@ng-icons/core';
-import { lucideArrowUpRight, lucideMenu, lucideX } from '@ng-icons/lucide';
+import {
+  lucideArrowUpRight,
+  lucideChevronsDown,
+  lucideCode,
+  lucideGithub,
+  lucideMenu,
+  lucideSchool,
+  lucideX,
+  lucideExternalLink,
+} from '@ng-icons/lucide';
 
-export const icons = { lucideArrowUpRight, lucideMenu, lucideX };
+export const icons = {
+  lucideArrowUpRight,
+  lucideMenu,
+  lucideX,
+  lucideChevronsDown,
+  lucideCode,
+  lucideSchool,
+  lucideGithub,
+  lucideExternalLink,
+};
 
 export const provideAppIcons = provideIcons(icons);

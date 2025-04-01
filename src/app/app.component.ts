dd8aac45c162c1a730fcf4d714fa1f7b37188e7f
thulanigamtee <thulanigamtee@gmail.com>
2025-04-01 04:34:52 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { ThemeService } from './services/theme.service';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LogoComponent } from './shared/logo/logo.component';
+import { HlmIconDirective } from './shared/ui/ui-icon-helm/src/lib/hlm-icon.directive';
+import { NgIcon } from '@ng-icons/core';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   imports: [
@@ -15,6 +20,11 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
     HomeComponent,
     AboutComponent,
     PortfolioComponent,
+    ContactComponent,
+    LogoComponent,
+    HlmIconDirective,
+    NgIcon,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
 })

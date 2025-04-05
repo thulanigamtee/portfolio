c7997517da9e6760b092b59edf04512b6f9bfe56
thulanigamtee <thulanigamtee@gmail.com>
2025-04-05 21:11:57 +0200

    update header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,6 +5,7 @@ import { NgIcon } from '@ng-icons/core';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 import { LogoComponent } from '../../shared/logo/logo.component';
 import { OverlayService } from './overlay.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -15,16 +16,54 @@ import { OverlayService } from './overlay.service';
     HlmIconDirective,
     LogoComponent,
     HlmButtonDirective,
+    NgClass,
   ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  overlayService = inject(OverlayService);
+  private overlayService = inject(OverlayService);
 
-  isMenuActive = false;
+  isHeaderHidden = false;
+  isScrolled = false;
+  isMobileMenuActive = false;
 
-  toggleMenu() {
-    this.isMenuActive = !this.isMenuActive;
-    this.overlayService.overlayState = this.isMenuActive;
+  ngOnInit() {
+    window.addEventListener('scroll', this.onScroll, true);
+    window.addEventListener('resize', this.onResize);
   }
+
+  toggleMobileMenu() {
+    this.isMobileMenuActive ? this.hideMobileMenu() : this.showMobileMenu();
+  }
+
+  showMobileMenu() {
+    this.isMobileMenuActive = true;
+    this.overlayService.overlayState = true;
+    document.body.classList.add('no-scroll');
+  }
+
+  hideMobileMenu() {
+    this.isMobileMenuActive = false;
+    this.overlayService.overlayState = false;
+    document.body.classList.remove('no-scroll');
+  }
+
+  onResize = () => {
+    const isDesktop = window.innerWidth >= 768;
+    if (isDesktop && this.isMobileMenuActive) {
+      this.hideMobileMenu();
+    }
+  };
+
+  lastScrollTop = 0;
+  onScroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > this.lastScrollTop && scrollTop > 100) {
+      this.isHeaderHidden = true;
+    } else this.isHeaderHidden = false;
+
+    this.isScrolled = scrollTop > 0;
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  };
 }

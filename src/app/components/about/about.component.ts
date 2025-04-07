a4c0762fc562603d4b77e309eb86820ad0811233
thulanigamtee <thulanigamtee@gmail.com>
2025-04-07 05:22:39 +0200

    update about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -14,6 +14,6 @@ export class AboutComponent {
     subtitle: 'Introduction',
     title: 'About me',
     description:
-      'I am an experienced Full Stack Software Developer with almost three years of expertise in the field. Throughout my career, I have had the opportunity to work on a variety of projects, which has allowed me to develop a wide range of skills. I am passionate about creating high-quality software and am always looking for ways to improve my skills.',
+      'I am an experienced Full Stack Software Developer with three years of expertise in the field. Throughout my career, I have had the opportunity to work on a variety of projects, which has allowed me to develop a wide range of skills. I am passionate about creating high-quality software and am always looking for ways to improve my skills.',
   };
 }

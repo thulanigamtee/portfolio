5ff5a0c6bc00ae06893a7ef63670061abd89e9fe
thulanigamtee <thulanigamtee@gmail.com>
2025-04-07 05:23:22 +0200

    update footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 import { HlmIconDirective } from '../../shared/ui/ui-icon-helm/src/lib/hlm-icon.directive';
 import { NgIcon } from '@ng-icons/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmTooltipTriggerDirective } from '@spartan-ng/ui-tooltip-helm';
 
+interface Socials {
+  name: string;
+  link: string;
+  icon: string;
+}
 @Component({
   selector: 'app-footer',
-  imports: [HlmIconDirective, NgIcon],
+  imports: [
+    HlmIconDirective,
+    NgIcon,
+    HlmButtonDirective,
+    HlmTooltipTriggerDirective,
+  ],
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  socials: Socials[] = [
+    {
+      name: 'Linkedin',
+      link: 'https://za.linkedin.com/in/thulani-gamteni-8845ab207',
+      icon: 'lucideLinkedin',
+    },
+    {
+      name: 'Github',
+      link: 'https://github.com/thulanigamtee',
+      icon: 'lucideGithub',
+    },
+  ];
+}
